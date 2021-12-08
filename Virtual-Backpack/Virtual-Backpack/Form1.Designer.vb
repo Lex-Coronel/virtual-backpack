cd8b87b409ff70be3bddcacc0bcd484ee744e680
crimsonfangs13 <jvranopa@gmail.com>
2021-12-08 21:05:51 +0800

    Ako lang to

--- Virtual-Backpack/Virtual-Backpack/Form1.Designer.vb
+++ Virtual-Backpack/Virtual-Backpack/Form1.Designer.vb
@@ -25,43 +25,58 @@ Partial Class Form1
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(367, 141)
+        Me.Label1.Location = New System.Drawing.Point(489, 174)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(28, 13)
+        Me.Label1.Size = New System.Drawing.Size(36, 17)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Test"
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(281, 178)
+        Me.Button1.Location = New System.Drawing.Point(375, 219)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(199, 49)
+        Me.Button1.Size = New System.Drawing.Size(265, 60)
         Me.Button1.TabIndex = 1
         Me.Button1.Text = "Test"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(231, 276)
+        Me.Button2.Location = New System.Drawing.Point(308, 340)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(290, 85)
+        Me.Button2.Size = New System.Drawing.Size(387, 105)
         Me.Button2.TabIndex = 2
         Me.Button2.Text = "Button2"
         Me.Button2.UseVisualStyleBackColor = True
         '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(747, 190)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(202, 63)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "ako lang to"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1067, 554)
+        Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
@@ -72,4 +87,5 @@ Partial Class Form1
     Friend WithEvents Label1 As Label
     Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
 End Class
