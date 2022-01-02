97ea6fb10d495089566f83beb96f837551977a7c
crimsonfangs13 <jvranopa@gmail.com>
2022-01-02 15:16:22 +0800

    Loginready

--- Virtual-Backpack/Virtual-Backpack/homepage.Designer.vb
+++ Virtual-Backpack/Virtual-Backpack/homepage.Designer.vb
@@ -25,7 +25,7 @@ Partial Class homepage
         Me.components = New System.ComponentModel.Container()
         Me.Guna2Elipse1 = New Guna.UI2.WinForms.Guna2Elipse(Me.components)
         Me.Guna2GradientButton1 = New Guna.UI2.WinForms.Guna2GradientButton()
-        Me.Guna2TextBox1 = New Guna.UI2.WinForms.Guna2TextBox()
+        Me.usertb = New Guna.UI2.WinForms.Guna2TextBox()
         Me.Guna2GradientButton2 = New Guna.UI2.WinForms.Guna2GradientButton()
         Me.Guna2GradientButton3 = New Guna.UI2.WinForms.Guna2GradientButton()
         Me.Guna2PictureBox2 = New Guna.UI2.WinForms.Guna2PictureBox()
@@ -44,7 +44,7 @@ Partial Class homepage
         Me.Guna2GradientButton1.Animated = True
         Me.Guna2GradientButton1.AutoRoundedCorners = True
         Me.Guna2GradientButton1.BackColor = System.Drawing.Color.Transparent
-        Me.Guna2GradientButton1.BorderRadius = 21
+        Me.Guna2GradientButton1.BorderRadius = 26
         Me.Guna2GradientButton1.CheckedState.Parent = Me.Guna2GradientButton1
         Me.Guna2GradientButton1.CustomImages.Image = Global.Virtual_Backpack.My.Resources.Resources.notepad
         Me.Guna2GradientButton1.CustomImages.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left
@@ -66,47 +66,48 @@ Partial Class homepage
         Me.Guna2GradientButton1.HoverState.FillColor2 = System.Drawing.Color.FromArgb(CType(CType(245, Byte), Integer), CType(CType(139, Byte), Integer), CType(CType(38, Byte), Integer))
         Me.Guna2GradientButton1.HoverState.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Guna2GradientButton1.HoverState.Parent = Me.Guna2GradientButton1
-        Me.Guna2GradientButton1.Location = New System.Drawing.Point(62, 98)
+        Me.Guna2GradientButton1.Location = New System.Drawing.Point(83, 121)
+        Me.Guna2GradientButton1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Guna2GradientButton1.Name = "Guna2GradientButton1"
         Me.Guna2GradientButton1.ShadowDecoration.Parent = Me.Guna2GradientButton1
-        Me.Guna2GradientButton1.Size = New System.Drawing.Size(153, 45)
+        Me.Guna2GradientButton1.Size = New System.Drawing.Size(204, 55)
         Me.Guna2GradientButton1.TabIndex = 3
         Me.Guna2GradientButton1.Text = "Notepad"
         Me.Guna2GradientButton1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
         Me.Guna2GradientButton1.UseTransparentBackground = True
         '
-        'Guna2TextBox1
+        'usertb
         '
-        Me.Guna2TextBox1.BorderThickness = 0
-        Me.Guna2TextBox1.Cursor = System.Windows.Forms.Cursors.IBeam
-        Me.Guna2TextBox1.DefaultText = "Hello, User!"
-        Me.Guna2TextBox1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer))
-        Me.Guna2TextBox1.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer))
-        Me.Guna2TextBox1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
-        Me.Guna2TextBox1.DisabledState.Parent = Me.Guna2TextBox1
-        Me.Guna2TextBox1.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
-        Me.Guna2TextBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
-        Me.Guna2TextBox1.FocusedState.Parent = Me.Guna2TextBox1
-        Me.Guna2TextBox1.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold)
-        Me.Guna2TextBox1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
-        Me.Guna2TextBox1.HoverState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
-        Me.Guna2TextBox1.HoverState.Parent = Me.Guna2TextBox1
-        Me.Guna2TextBox1.Location = New System.Drawing.Point(616, 12)
-        Me.Guna2TextBox1.Margin = New System.Windows.Forms.Padding(4)
-        Me.Guna2TextBox1.Name = "Guna2TextBox1"
-        Me.Guna2TextBox1.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
-        Me.Guna2TextBox1.PlaceholderText = ""
-        Me.Guna2TextBox1.SelectedText = ""
-        Me.Guna2TextBox1.ShadowDecoration.Parent = Me.Guna2TextBox1
-        Me.Guna2TextBox1.Size = New System.Drawing.Size(123, 40)
-        Me.Guna2TextBox1.TabIndex = 6
+        Me.usertb.BorderThickness = 0
+        Me.usertb.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.usertb.DefaultText = "Hello, User!"
+        Me.usertb.DisabledState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer))
+        Me.usertb.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer))
+        Me.usertb.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
+        Me.usertb.DisabledState.Parent = Me.usertb
+        Me.usertb.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
+        Me.usertb.FocusedState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.usertb.FocusedState.Parent = Me.usertb
+        Me.usertb.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.usertb.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.usertb.HoverState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.usertb.HoverState.Parent = Me.usertb
+        Me.usertb.Location = New System.Drawing.Point(818, 14)
+        Me.usertb.Margin = New System.Windows.Forms.Padding(5, 5, 5, 5)
+        Me.usertb.Name = "usertb"
+        Me.usertb.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.usertb.PlaceholderText = ""
+        Me.usertb.SelectedText = ""
+        Me.usertb.ShadowDecoration.Parent = Me.usertb
+        Me.usertb.Size = New System.Drawing.Size(164, 49)
+        Me.usertb.TabIndex = 6
         '
         'Guna2GradientButton2
         '
         Me.Guna2GradientButton2.Animated = True
         Me.Guna2GradientButton2.AutoRoundedCorners = True
         Me.Guna2GradientButton2.BackColor = System.Drawing.Color.Transparent
-        Me.Guna2GradientButton2.BorderRadius = 21
+        Me.Guna2GradientButton2.BorderRadius = 26
         Me.Guna2GradientButton2.CheckedState.Parent = Me.Guna2GradientButton2
         Me.Guna2GradientButton2.CustomImages.Image = Global.Virtual_Backpack.My.Resources.Resources.planner
         Me.Guna2GradientButton2.CustomImages.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left
@@ -128,10 +129,11 @@ Partial Class homepage
         Me.Guna2GradientButton2.HoverState.FillColor2 = System.Drawing.Color.FromArgb(CType(CType(245, Byte), Integer), CType(CType(139, Byte), Integer), CType(CType(38, Byte), Integer))
         Me.Guna2GradientButton2.HoverState.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Guna2GradientButton2.HoverState.Parent = Me.Guna2GradientButton2
-        Me.Guna2GradientButton2.Location = New System.Drawing.Point(62, 142)
+        Me.Guna2GradientButton2.Location = New System.Drawing.Point(83, 175)
+        Me.Guna2GradientButton2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Guna2GradientButton2.Name = "Guna2GradientButton2"
         Me.Guna2GradientButton2.ShadowDecoration.Parent = Me.Guna2GradientButton2
-        Me.Guna2GradientButton2.Size = New System.Drawing.Size(153, 45)
+        Me.Guna2GradientButton2.Size = New System.Drawing.Size(204, 55)
         Me.Guna2GradientButton2.TabIndex = 8
         Me.Guna2GradientButton2.Text = "Daily Planner"
         Me.Guna2GradientButton2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
@@ -142,7 +144,7 @@ Partial Class homepage
         Me.Guna2GradientButton3.Animated = True
         Me.Guna2GradientButton3.AutoRoundedCorners = True
         Me.Guna2GradientButton3.BackColor = System.Drawing.Color.Transparent
-        Me.Guna2GradientButton3.BorderRadius = 21
+        Me.Guna2GradientButton3.BorderRadius = 26
         Me.Guna2GradientButton3.CheckedState.Parent = Me.Guna2GradientButton3
         Me.Guna2GradientButton3.CustomImages.Image = Global.Virtual_Backpack.My.Resources.Resources.logout
         Me.Guna2GradientButton3.CustomImages.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left
@@ -164,10 +166,11 @@ Partial Class homepage
         Me.Guna2GradientButton3.HoverState.FillColor2 = System.Drawing.Color.FromArgb(CType(CType(245, Byte), Integer), CType(CType(139, Byte), Integer), CType(CType(38, Byte), Integer))
         Me.Guna2GradientButton3.HoverState.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Guna2GradientButton3.HoverState.Parent = Me.Guna2GradientButton3
-        Me.Guna2GradientButton3.Location = New System.Drawing.Point(62, 374)
+        Me.Guna2GradientButton3.Location = New System.Drawing.Point(83, 460)
+        Me.Guna2GradientButton3.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Guna2GradientButton3.Name = "Guna2GradientButton3"
         Me.Guna2GradientButton3.ShadowDecoration.Parent = Me.Guna2GradientButton3
-        Me.Guna2GradientButton3.Size = New System.Drawing.Size(153, 45)
+        Me.Guna2GradientButton3.Size = New System.Drawing.Size(204, 55)
         Me.Guna2GradientButton3.TabIndex = 9
         Me.Guna2GradientButton3.Text = "Log out"
         Me.Guna2GradientButton3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
@@ -177,16 +180,18 @@ Partial Class homepage
         '
         Me.Guna2PictureBox2.Image = Global.Virtual_Backpack.My.Resources.Resources.user_home_1
         Me.Guna2PictureBox2.ImageRotate = 0!
-        Me.Guna2PictureBox2.Location = New System.Drawing.Point(743, 12)
+        Me.Guna2PictureBox2.Location = New System.Drawing.Point(991, 15)
+        Me.Guna2PictureBox2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Guna2PictureBox2.Name = "Guna2PictureBox2"
         Me.Guna2PictureBox2.ShadowDecoration.Parent = Me.Guna2PictureBox2
-        Me.Guna2PictureBox2.Size = New System.Drawing.Size(40, 40)
+        Me.Guna2PictureBox2.Size = New System.Drawing.Size(53, 49)
         Me.Guna2PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.Guna2PictureBox2.TabIndex = 7
         Me.Guna2PictureBox2.TabStop = False
         '
         'Guna2ContextMenuStrip1
         '
+        Me.Guna2ContextMenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
         Me.Guna2ContextMenuStrip1.Name = "Guna2ContextMenuStrip1"
         Me.Guna2ContextMenuStrip1.RenderStyle.ArrowColor = System.Drawing.Color.FromArgb(CType(CType(151, Byte), Integer), CType(CType(143, Byte), Integer), CType(CType(255, Byte), Integer))
         Me.Guna2ContextMenuStrip1.RenderStyle.BorderColor = System.Drawing.Color.Gainsboro
@@ -203,31 +208,33 @@ Partial Class homepage
         '
         Me.Guna2PictureBox1.Image = Global.Virtual_Backpack.My.Resources.Resources.gradient_background2
         Me.Guna2PictureBox1.ImageRotate = 0!
-        Me.Guna2PictureBox1.Location = New System.Drawing.Point(-8, -2)
+        Me.Guna2PictureBox1.Location = New System.Drawing.Point(-11, -2)
+        Me.Guna2PictureBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Guna2PictureBox1.Name = "Guna2PictureBox1"
         Me.Guna2PictureBox1.ShadowDecoration.Color = System.Drawing.Color.DarkGray
         Me.Guna2PictureBox1.ShadowDecoration.Parent = Me.Guna2PictureBox1
-        Me.Guna2PictureBox1.Size = New System.Drawing.Size(223, 456)
+        Me.Guna2PictureBox1.Size = New System.Drawing.Size(297, 561)
         Me.Guna2PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.Guna2PictureBox1.TabIndex = 2
         Me.Guna2PictureBox1.TabStop = False
         '
         'homepage
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.White
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1067, 554)
         Me.Controls.Add(Me.Guna2GradientButton3)
         Me.Controls.Add(Me.Guna2GradientButton2)
         Me.Controls.Add(Me.Guna2PictureBox2)
-        Me.Controls.Add(Me.Guna2TextBox1)
+        Me.Controls.Add(Me.usertb)
         Me.Controls.Add(Me.Guna2GradientButton1)
         Me.Controls.Add(Me.Guna2PictureBox1)
         Me.DoubleBuffered = True
         Me.ForeColor = System.Drawing.SystemColors.ControlText
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "homepage"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "homepage"
@@ -240,7 +247,7 @@ Partial Class homepage
     Friend WithEvents Guna2Elipse1 As Guna.UI2.WinForms.Guna2Elipse
     Friend WithEvents Guna2GradientButton1 As Guna.UI2.WinForms.Guna2GradientButton
     Friend WithEvents Guna2PictureBox2 As Guna.UI2.WinForms.Guna2PictureBox
-    Friend WithEvents Guna2TextBox1 As Guna.UI2.WinForms.Guna2TextBox
+    Friend WithEvents usertb As Guna.UI2.WinForms.Guna2TextBox
     Friend WithEvents Guna2GradientButton2 As Guna.UI2.WinForms.Guna2GradientButton
     Friend WithEvents Guna2GradientButton3 As Guna.UI2.WinForms.Guna2GradientButton
     Friend WithEvents Guna2PictureBox1 As Guna.UI2.WinForms.Guna2PictureBox
--- Virtual-Backpack/Virtual-Backpack/registration.vb
+++ Virtual-Backpack/Virtual-Backpack/registration.vb
@@ -1,5 +1,45 @@
-﻿Public Class registration
+﻿Imports MySql.Data.MySqlClient
+Public Class registration
+    Dim MysqlConn As MySqlConnection
+    Dim COMMAND As MySqlCommand
     Private Sub registration_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub loginBtn_Click(sender As Object, e As EventArgs) Handles loginBtn.Click
+        MysqlConn = New MySqlConnection()
+        'Change userid and password
+        '///////////////////////////////////////////////////////////////////////////////////
+        MysqlConn.ConnectionString =
+            "server=localhost;userid=root;password=crimsonfangs13;database=virtualbackpack"
+        '//////////////////////////////////////////////////////////////////////////////////
+        Dim READER As MySqlDataReader
+        If passwordTb.Text = confirmTB.Text Then
+            Try
+                MysqlConn.Open()
+                Dim Query As String
+                'Database name,Table name and Column name
+                '//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                Query = "INSERT INTO virtualbackpack.accounts(username,password) VALUES ('" & userTb.Text & "','" & passwordTb.Text & "')"
+                '//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                COMMAND = New MySqlCommand(Query, MysqlConn)
+                READER = COMMAND.ExecuteReader
+                MessageBox.Show("Account Successfully Created!")
+
+                MysqlConn.Close()
+
+
+            Catch ex As Exception
+                MessageBox.Show(ex.Message)
+            Finally
+                Me.Hide()
+                login.Show()
+                MysqlConn.Dispose()
+
+            End Try
+        Else
+            MessageBox.Show("Password does not match.")
+        End If
 
     End Sub
 End Class
